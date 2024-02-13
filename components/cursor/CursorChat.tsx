a125695c8dd1379d63d6260f6a60b1b2f7326245
kevin <kthurman59@gmail.com>
2024-02-13 10:35:26 -0600

    logic update

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import CursorSVG from '@/public/assets/CursorSVG'
+import { CursorChatProps, CursorMode } from '@/types/type'
 
-const CursorChat = () => {
+//{cursorState.mode === CursorMode.Chat && (
+//  )}
+
+const CursorChat = ({ cursor, cursorState, setCursorState, updateMyPresence }: CursorChatProps) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    
+  }
+
   return (
-    <div>CursorChat</div>
+    <div className='absolute top-0 left-0' style={{
+      transform: `translatex(${cursor.x}px) translatey(${cursor.y}px)`}}>
+          <>
+            <CursorSVG color="#000" />
+
+            <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
+              {cursorState.previousMessage && (
+                <div>{cursorState.previousMessage}</div>
+              )}
+              <input
+                className='z-10 w-60 border-none bg-transparent text-white placeholder-blue-300 outline-none'
+                autoFocus={true} 
+              />
+            </div>
+          </>
+    </div>
   )
 }
 
