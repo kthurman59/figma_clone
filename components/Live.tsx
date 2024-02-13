b37a38b52f099d220a0610847837277bcc2e1f4d
kevin <kthurman59@gmail.com>
2024-02-13 05:37:51 -0600

    cursor updating

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,10 +1,16 @@
 import { useMyPresence, useOthers } from "@/liveblocks.config"
 import LiveCursors from "./cursor/LiveCursors"
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
+import CursorChat from "./cursor/CursorChat";
+import { CursorMode } from "@/types/type";
 
 const Live = () => {
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  const [cursorState, setCursorState] = useState({
+    mode: CursorMode.Hidden, 
+  })
 
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
         event.preventDefault();
@@ -16,8 +22,7 @@ const Live = () => {
   }, [])
 
   const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-    event.preventDefault();
-
+    setCursorState({ mode: CursorMode.Hidden })
     updateMyPresence({ cursor: null, message: null });
 }, [])
 
@@ -36,6 +41,15 @@ const handlePointerDown = useCallback((event: React.PointerEvent) => {
         className="h-[100vh] w-full flex justify-center items-center text-center"
     >
         <h1 className="font-2xl text-white">LiveBlocks Figma Clone </h1>
+
+        {cursor && (
+          <CursorChat 
+            cursor={cursor}
+            cursorState={cursorState}
+            setCursorState={setCursorState}
+            updateMyPresence={updateMyPresence}
+          />
+        )}
 
         <LiveCursors others={others} />
     </div>
